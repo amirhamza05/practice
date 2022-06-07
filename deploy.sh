a4a8debb38e0d459d512a1d0698546eb0f5ecbdd
Sk.Amir Hamza <sk.amirhamza@gmail.com>
2022-06-08 01:44:57 +0600

    Update and rename test.sh to deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+echo '============================DEPLOYING >>> DEVELOPMENT==========================='
+cd deploy
+echo "Hello World" > test.txt
+date  > date.txt
+echo '=========== Done ==================='
